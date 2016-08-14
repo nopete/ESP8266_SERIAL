51db6cfb724dfd312dca7f69b4a1b7d03f4f78af
nopete <peter.novikov@gmail.com>
2016-08-15 01:19:20 +0300

    baudrate for soft serial

--- ESP8266_SERIAL.ino
+++ ESP8266_SERIAL.ino
@@ -1,45 +1,34 @@
-// FROM http://allaboutee.com/2014/12/27/esp8266-arduino-code-and-circuit/
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(8, 9); // RX, TX
 
-    #include <SoftwareSerial.h>
-     
-    SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
-                                 // This means that you need to connect the TX line from the esp to the Arduino's pin 2
-                                 // and the RX line from the esp to the Arduino's pin 3
-    void setup()
-    {
-      Serial.begin(9600);
-      esp8266.begin(115200); // your esp's baud rate might be different
-    }
-     
-    void loop()
-    {
-      if(esp8266.available()) // check if the esp is sending a message 
-      {
-        while(esp8266.available())
-        {
-          // The esp has data so display its output to the serial window 
-          char c = esp8266.read(); // read the next character.
-          Serial.write(c);
-        }  
-      }
-      
-     
-      
-      if(Serial.available())
-      {
-        // the following delay is required because otherwise the arduino will read the first letter of the command but not the rest
-        // In other words without the delay if you use AT+RST, for example, the Arduino will read the letter A send it, then read the rest and send it
-        // but we want to send everything at the same time.
-        delay(1000); 
-        
-        String command="";
-        
-        while(Serial.available()) // read the command character by character
-        {
-            // read one character
-          command+=(char)Serial.read();
-        }
-        esp8266.println(command); // send the read character to the esp8266
-      }
-    }
+void setup() 
+{
+  uint32_t baud = 9600;
+  Serial.begin(9600);
+  softSerial.begin(115200);
+  softSerial.print("AT+CIOBAUD=");
+  softSerial.println(baud);
+  softSerial.begin(baud);
+  Serial.print("SETUP!! @");
+  Serial.println(baud);
+}
 
+void loop() 
+{
+    while(softSerial.available() > 0) 
+    {
+      char a = softSerial.read();
+      if(a == '\0')
+        continue;
+      if(a != '\r' && a != '\n' && (a < 32))
+        continue;
+      Serial.print(a);
+    }
+    
+    while(Serial.available() > 0)
+    {
+      char a = Serial.read();
+      Serial.write(a);
+      softSerial.write(a);
+    }
+}
